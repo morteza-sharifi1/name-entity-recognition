c04cc86c06a104023d5094d3201d35fc59f4835a
Morteza Sharifi <121801238+morteza-sharifi1@users.noreply.github.com>
2023-08-06 04:27:42 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st 
+import spacy
+import spacy_streamlit as spt
+
+nlp = spacy.load('en_core_web_sm')
+
+def main():
+	st.title('Name Entity Recognition')
+	menu = ['Home', 'NER']
+	choice = st.sidebar.selectbox('menu', menu)
+	if choice == 'Home':
+		st.subheader('Word Tokenization')
+		raw_text = st.text_area('Text To Tokenize', 'Enter Text Here')
+		docs = nlp(raw_text)
+		if st.button('Tokenize'):
+			spt.visualize_tokens(docs)
+
+	elif choice == 'NER':
+		st.subheader('Name Entity Recognition')
+		raw_text = st.text_area('Text To Tokenize', 'Enter Text Here')
+		docs = nlp(raw_text)
+		spt.visualize_ner(docs)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
